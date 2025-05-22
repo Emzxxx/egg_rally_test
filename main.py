c8ff170e3642bb0d538f87fa73fdb6d022e2cd49
Ryan Dela Cruz <rbdelacruz12@up.edu.ph>
2025-05-22 19:15:01 +0800

    feat: add defeated eggnemies counter and hp stat

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ import random
 with open("settings.json") as f:
     settings = json.load(f)
 
+# Global variables
 i_frame: int = settings["fps"]
+egg_range: int = 10
+eggnemies_defeated: int = 0
 
 class Egg(ABC):
     def __init__(self, x: int, y: int, width: int, height: int, hp: int):
@@ -37,27 +40,6 @@ class Eggnemy(Egg):
     def __init__(self, x: int, y: int, width: int, height: int, hp: int):
         super().__init__(x, y, width, height, hp)
 
-egg = Egg(
-    settings["world_width"] // 2,
-    settings["world_height"] // 2,
-    settings["egg_width"],
-    settings["egg_height"],
-    settings["egg_hp"]
-)
-
-egg_range = 10
-
-enemies: list[Eggnemy] = [
-    Eggnemy(
-        random.randint(0, settings["world_width"]),
-        random.randint(0, settings["world_height"]),
-        settings["eggnemy_width"],
-        settings["eggnemy_height"],
-        1,
-    )
-    for _ in range(settings["eggnemy_count"])
-]
-
 def is_in_collision(egg: Egg, enemy: Eggnemy) -> bool:
     if egg.right < enemy.left:
         return False
@@ -84,8 +66,32 @@ def is_in_range(egg: Egg, enemy: Eggnemy) -> bool:
         else:
             return True
 
+def remove_enemy(enemy: Eggnemy, Eggnemies: list[Eggnemy]):
+    Eggnemies.remove(enemy)
+
+# Initialize entities
+egg = Egg(
+    settings["world_width"] // 2,
+    settings["world_height"] // 2,
+    settings["egg_width"],
+    settings["egg_height"],
+    settings["egg_hp"]
+)
+
+enemies: list[Eggnemy] = [
+    Eggnemy(
+        random.randint(0, settings["world_width"]),
+        random.randint(0, settings["world_height"]),
+        settings["eggnemy_width"],
+        settings["eggnemy_height"],
+        1,
+    )
+    for _ in range(settings["eggnemy_count"])
+]
+
 def update():
     global i_frame
+    global eggnemies_defeated
 
     speed = 2
 
@@ -124,28 +130,39 @@ def update():
     if pyxel.btn(pyxel.KEY_L):
         for enemy in enemies[:]:
             if is_in_range(egg, enemy):
-                enemies.remove(enemy)
+                enemy.hp -= 1
+                remove_enemy(enemy, enemies)
+                eggnemies_defeated += 1
+                print(eggnemies_defeated)
+
+def draw_egg(egg: Egg):
+    pyxel.rect(egg.x, egg.y, egg.width, egg.height, 7)
+
+def draw_egg_hp(egg: Egg):
+    pyxel.text(egg.x - 5, egg.y + 10, f"{egg.hp}/{egg.max_hp}", 7)
 
 def draw_range(egg: Egg):
     pyxel.rectb(egg.x - egg_range, egg.y - egg_range, egg.width + 2 * egg_range, egg.height + 2 * egg_range, 1)
-def draw_hp(egg: Egg):
-    pyxel.text(egg.x - 5, egg.y + 10, f"{egg.hp}/10", 7)
-def draw_egg(egg: Egg):
-    if egg.hp > 0:
-        pyxel.rect(egg.x, egg.y, egg.width, egg.height, 7)
-        draw_range(egg)
-        draw_hp(egg)
-
-
 
 def draw_eggnemies(enemies: list[Eggnemy]) -> None:
     for enemy in enemies:
         pyxel.rect(enemy.x, enemy.y, enemy.width, enemy.height, 8)
 
+def draw_eggnemies_defeated(eggnemies_defeated: int):
+    pyxel.text(10, 10, f'{eggnemies_defeated}', 7)
+
+def draw_eggnemies_hp(enemies: list[Eggnemy]) -> None:
+    for enemy in enemies:
+        pyxel.text(enemy.x - 5, enemy.y + 10, f"{enemy.hp}/{enemy.max_hp}", 7)
+
 def draw():
     pyxel.cls(0)
     draw_egg(egg)
+    draw_egg_hp(egg)
+    draw_range(egg)
     draw_eggnemies(enemies)
+    draw_eggnemies_defeated(eggnemies_defeated)
+    draw_eggnemies_hp(enemies)
 
 def main():
     pyxel.init(settings["world_width"], settings["world_height"], fps=settings["fps"])
